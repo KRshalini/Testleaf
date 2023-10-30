dc9fb26d4414073258431d3c3af6555818a0ae06
KRshalini <114220850+KRshalini@users.noreply.github.com>
2023-10-30 20:24:18 +0530

    Update Amazon.java

--- Amzon.java
+++ Amzon.java
@@ -1,62 +1,68 @@
-package Assignment2;
+package assignments3;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Amzon {
-	public static void main(String[] args) throws InterruptedException {
-		
-//		1.Load the URL https://www.amazon.in/
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-notifications","start-maximized");
-		ChromeDriver driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+public class Amazon {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		//.Load the URL https://www.amazon.in/
+		ChromeDriver driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
 		
-//		2.search as oneplus 9 pro 
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("onrplus 9 pro",Keys.ENTER);
-		
-//		3.Get the price of the first product
-		String firstProductPrice = driver.findElement(By.xpath("//span[@class='a-price-whole']")).getText();
-		System.out.println(firstProductPrice);
-		
-//		4. Print the number of customer ratings for the first displayed product
-		String customerRatings = driver.findElement(By.xpath("//span[@class='a-size-base s-underline-text']"))
-				.getText();
-		System.out.println("Number of customer rating"+customerRatings);
-		
-//		5. Click the first text link of the first image
-		driver.findElement(By.xpath("//h2[contains(@class,'a-color-base s-line-clamp-2')]")).click();
-		
-//		6. Take a screen shot of the product displayed
-		
-//		7. Click 'Add to Cart' button
-		Set<String> newWindow = driver.getWindowHandles();
-		List<String> childWindow = new ArrayList<String>(newWindow);
-		
-	    driver.switchTo().window(childWindow.get(1));
-		driver.findElement(By.id("add-to-cart-button")).click();
-		
-//		8. Get the cart subtotal and verify if it is correct.
+		//2.search as oneplus 9 pro 
+		WebElement ele1 = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		ele1.sendKeys("oneplus 9 pro");
+		ele1.click();
+				
+		//3.Get the price of the first product
 		Thread.sleep(3000);
-		String cart = driver.findElement(By.xpath("//span[@id='attach-accessory-cart-subtotal']")).getText();
-		System.out.println(cart);
+		String firstproduct = driver.findElement(By.xpath("//span[contains(text(),'(Refurbished) OnePlus 9 Pro 5G')]")).getText();
+		System.out.println(firstproduct);
 		
-	/*	if(firstProductPrice==finalPrice) {
-			System.out.println("Price is validated");
-		}else {
-			System.out.println("Price is mismatching");
-		}
-		*/
-//		9.close the browser
-		driver.quit();
+		//4. Print the number of customer ratings for the first displayed product
+		String noOfCustomerratings = driver.findElement(By.xpath("//span[text()='2 out of 5']")).getText();
+		System.out.println("The customer ratings " +noOfCustomerratings);
+		
+		//5. Click the first text link of the first image
+		driver.findElement(By.xpath("//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']")).click();
+		
+		
+		//6. Take a screen shot of the product displayed
+		File snapshot = driver.getScreenshotAs(OutputType.FILE);
+        File snap = new File("./snap/002.jpg");
+        FileUtils.copyFile(snapshot, snap);
+        
+		//7. Click 'Add to Cart' button
+        Set<String> window = driver.getWindowHandles();
+        List<String> childWindow = new ArrayList<String>(window);
+        driver.switchTo().window(childWindow.get(1));
+        driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();
+        
+        
+		//8. Get the cart subtotal and verify if it is correct.
+        String carttotal = driver.findElement(By.xpath("//span[@id='attach-accessory-cart-subtotal']")).getText();
+        System.out.println(carttotal);
+        
+        if(carttotal==firstproduct) {
+        	System.out.println("Same and verifed");
+        }
+        else {
+        	System.out.println("different");
+        }
+		//9.close the browser
+        //driver.quit();
 	}
-	
+
 }
